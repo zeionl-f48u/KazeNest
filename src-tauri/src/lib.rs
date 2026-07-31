dab55a6b3bcf97840b4e3da2560765552f95f2ae
Zeionl <106791055+zeionl-f48u@users.noreply.github.com>
2026-07-31 13:51:13 +0800

    first commet

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,12 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
